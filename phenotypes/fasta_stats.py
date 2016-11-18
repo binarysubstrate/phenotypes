6bb80fc95e7aea65a84b0bc3a0c653dac5fb259c
binarysubstrate <mwelcks@gmail.com>
2016-11-17 20:33:24 -0500

    ENH: Create parameter for fasta filenames.
    Create parameter in each function for list of FASTA files.
    Improve output.
    Create variables for file sets used during initial runs.

--- phenotypes/fasta_stats.py
+++ phenotypes/fasta_stats.py
@@ -14,35 +14,26 @@ def load_fasta(filename):
     return lengths
 
 
-def print_seq_lengths():
-    file_names = [
-        'heritable.fasta', 'hsp104.fasta', 'hsp70.fasta', 'nc.fasta',
-        'orf_trans.fasta', 'overexpression_all.fasta']
-
+def print_seq_lengths(fasta_files):
     all_lengths = []
-    for file_name in file_names:
+    for file_name in fasta_files:
         file_lengths = load_fasta(os.path.join('data', file_name))
         all_lengths.extend(file_lengths)
 
     all_lengths.sort()
-    print(
-        "Mean of the smallest "
-        "100 sequences: {}".format(np.mean(all_lengths[:5])))
+    print("Mean of the smallest "
+          "100 sequences: {}".format(np.mean(all_lengths[:100])))
     print("Mean of the largest "
-          "100 sequences: {}".format(np.mean(all_lengths[-5:])))
+          "100 sequences: {}".format(np.mean(all_lengths[-100:])))
     print("Mean of all sequences: {}".format(np.mean(all_lengths)))
     print("Maximum sequence length: {}".format(all_lengths[-1]))
     print("Number of sequences across all files: {}".format(len(all_lengths)))
     return
 
 
-def print_possible_chars():
-    file_names = [
-        'heritable.fasta', 'hsp104.fasta', 'hsp70.fasta', 'nc.fasta',
-        'orf_trans.fasta', 'overexpression_all.fasta']
-
+def print_possible_chars(fasta_files):
     all_chars = set()
-    for file_name in file_names:
+    for file_name in fasta_files:
         with open(os.path.join('data', file_name), 'r') as handle:
             for record in SeqIO.parse(handle, 'fasta'):
                 for char in record.seq:
@@ -53,19 +44,33 @@ def print_possible_chars():
 
     return None
 
-def localization_stats():
-    fastas = ['chloroplast.fasta', 'cytoplasmic.fasta', 'ER.fasta',
-              'extracellular.fasta', 'Golgi.fasta', 'nuclear.fasta',
-              'peroxisomal.fasta', 'plasma_membrane.fasta', 'vacuolar.fasta']
+
+def localization_stats(fasta_files):
+    print("Localization stats:")
     total = 0
-    for fasta in fastas:
+    for fasta in fasta_files:
         total += len(load_fasta(os.path.join('data/', fasta)))
         print(len(load_fasta(os.path.join('data/', fasta))))
-    print(1411/total)
+    print(1411 / total)
 
 
 def main():
-    localization_stats()
+    fasta_set1 = [
+        'heritable.fasta', 'hsp104.fasta', 'hsp70.fasta', 'nc.fasta',
+        'orf_trans.fasta', 'overexpression_all.fasta'
+    ]
+
+    fasta_set2 = [
+        'chloroplast.fasta', 'cytoplasmic.fasta', 'ER.fasta',
+        'extracellular.fasta', 'Golgi.fasta', 'nuclear.fasta',
+        'peroxisomal.fasta', 'plasma_membrane.fasta', 'vacuolar.fasta'
+    ]
+
+    localization_stats(fasta_set2)
+    print("\n{}\n".format("-"*30))
+    print_possible_chars(fasta_set2)
+    print("\n{}\n".format("-"*30))
+    print_seq_lengths(fasta_set2)
 
 
 if __name__ == '__main__':
